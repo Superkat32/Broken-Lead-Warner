499ade1999dce61dca9202fb8e8f4ce2a0fd5ec3
Superkat32 <superkat1806@gmail.com>
2022-07-02 21:18:30 -0400

    Initial commit

---
+++ src/main/java/net/superkat/tutorialmod/TutorialMod.java
@@ -0,0 +1,14 @@
+package net.superkat.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID = "tutorialmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
