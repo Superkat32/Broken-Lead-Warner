eb1da9fcb8b9fcdb22e4ef155be9e88cb407f6a8
Superkat32 <superkat1806@gmail.com>
2022-07-06 17:38:35 -0400

    Changed all text to be both bold and red.

--- src/main/java/net/superkat/tutorialmod/mixin/ExampleMixin.java
+++ src/main/java/net/superkat/tutorialmod/mixin/ExampleMixin.java
@@ -3,6 +3,7 @@ package net.superkat.tutorialmod.mixin;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.superkat.tutorialmod.TutorialMod;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -33,14 +34,14 @@ public class ExampleMixin {
 		if (actionBar) {
 			//Activates if the action bar setting is true.
 			//This piece of code will warn the player that their lead has broken right above their hotbar(aka. the action bar)
-			getInstance().inGameHud.setOverlayMessage(Text.of("Your lead has broken!"), false);
+			getInstance().inGameHud.setOverlayMessage(Text.literal("Your lead has broken!").formatted(Formatting.BOLD, Formatting.RED), false);
 			actionBar = false; //temp
 		} else {
 			//This piece of code will warn the player with a big title message
 			//There are 20 ticks a second
 			//It will take 0.5 seconds for the title to fade in, stay on screen for 40 seconds, then fade out in 0.75 seconds.
 			getInstance().inGameHud.setTitleTicks(10, 40, 15);
-			getInstance().inGameHud.setTitle(Text.of("Your lead broke!"));
+			getInstance().inGameHud.setTitle(Text.literal("Your lead broke!").formatted(Formatting.BOLD, Formatting.RED));
 			actionBar = true; //temp
 		}
 	}
