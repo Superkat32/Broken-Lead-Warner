cf9174294d0f59790c0efcda2facc4fdc7a91307
Superkat32 <superkat1806@gmail.com>
2022-08-14 19:40:12 -0400

    Changed code. Still needs to be worked on, but at least I can rollback if needed.

--- src/main/java/net/superkat/tutorialmod/mixin/ExampleMixin.java
+++ src/main/java/net/superkat/tutorialmod/mixin/ExampleMixin.java
@@ -1,26 +1,15 @@
 package net.superkat.tutorialmod.mixin;
 
-import net.fabricmc.fabric.mixin.datagen.client.MinecraftClientMixin;
-import net.fabricmc.fabric.mixin.networking.accessor.MinecraftClientAccessor;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.sound.PositionedSoundInstance;
-import net.minecraft.client.sound.SoundContainer;
-import net.minecraft.client.sound.SoundExecutor;
-import net.minecraft.client.sound.SoundInstance;
-import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.mob.MobEntity;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvent;
-import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Position;
-import net.minecraft.world.World;
-import net.minecraft.world.WorldEvents;
 import net.superkat.tutorialmod.*;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -39,12 +28,20 @@ public abstract class ExampleMixin {
 	@Shadow
 	protected abstract void playHurtSound(DamageSource source);
 
+	@Shadow
+	@Nullable
+	private Entity holdingEntity;
+
+	@Shadow
+	private int holdingEntityId;
+
 	//	@Inject(at = @At("HEAD"), method = "detachLeash()")
 	@Inject(at = @At("HEAD"), method = "detachLeash")
 	public void init(boolean sendPacket, boolean dropItem, CallbackInfo info) {
 		MobEntity self = (MobEntity) (Object) this;
 		if (self.getHoldingEntity() != null) {
 			if (!self.world.isClient && dropItem) {
+//			if (mobEntity.isLeashed() && mobEntity.getHoldingEntity() == this)
 				if (LeadWarnerConfig.getInstance().enabled) {
 					sendWarningMessage();
 				} else if (!LeadWarnerConfig.getInstance().enabled) {
@@ -55,7 +52,6 @@ public abstract class ExampleMixin {
 				}
 //				TutorialMod.LOGGER.info("YOUR LEAD HAS HEREBY BEEN DECLARED; BROKEN!!!");
 //				playerEntity.sendSystemMessage(new LiteralMessage("Your lead has broken!"), Util.NIL_UUID);
-
 			}
 		}
 //		TutorialMod.LOGGER.info("This line is printed by an example mod mixin!");
@@ -63,32 +59,35 @@ public abstract class ExampleMixin {
 
 	private void sendWarningMessage() {
 		TutorialMod.LOGGER.info("YOUR LEAD HAS HEREBY BEEN DECLARED; BROKEN!!!");
-		//TODO - Change "actionBar" to Enum and allow different preferred warning methods. e.g. "Big text", "small text", "advancement toast", "just sound", "none".
-		//TODO - Allow the sound to be changed. Use an Enum?
-		//FIXME - Mod sends message when lead breaks while on fence post.
-		//Activates if the action bar setting is true
+		/*
+		FIXME - Mod sends message when lead breaks while on fence post.
+		FIXME - Sound will play even if the leash was on a fence
+		FIXME - Mod will completely break if you put a leash on a fence, then have another lead break, then break the tied lead
+		FIXME - Mod will amplify sound if multiple leads break
+		FIXME - Change the whole method on how the mod realizes that the lead is tied to a fence.
+		*/
 		playSoundEffect();
 		if (LeadWarnerConfig.getInstance().actionBar) {
+			//Activates if the action bar setting is true
 			//This piece of code will warn the player that their lead has broken right above their hotbar(aka. the action bar)
-			getInstance().inGameHud.setOverlayMessage(Text.literal("Your lead has broken!").formatted(Formatting.BOLD, Formatting.RED), false);
-			//Will play a pling sound when your lead breaks
-			//TODO - Add a custom warning sound
+			getInstance().inGameHud.setOverlayMessage(Text.literal("Your lead broke!").formatted(Formatting.BOLD, Formatting.RED), false);
 //			MinecraftClient.getInstance().getSoundManager().play(PositionedSoundInstance.master(SoundEvents.BLOCK_NOTE_BLOCK_PLING, 1.0F, 1.0F));
-		} else {
+		} else if (!LeadWarnerConfig.getInstance().actionBar) {
 			//This piece of code will warn the player with a big title message at the center of their screen
 			//There are 20 ticks a second
 			//It will take 0.5 seconds for the title to fade in, stay on screen for 40 seconds, then fade out in 0.75 seconds
 			getInstance().inGameHud.setTitleTicks(10, 40, 15);
 			getInstance().inGameHud.setTitle(Text.literal("Your lead broke!").formatted(Formatting.BOLD, Formatting.RED));
+		} else {
+			TutorialMod.LOGGER.info("No warning message sent: none of the boolean conditions were met.");
 		}
 	}
 
 	private void playSoundEffect() {
 		if (LeadWarnerConfig.getInstance().playSound) {
-			//Plays note block "pling" sound
+			//Plays warning sound
 //			MinecraftClient.getInstance().getSoundManager().play(PositionedSoundInstance.master(TutorialMod.MY_SOUND_EVENT, 1.0F, 1.0F));
 //			MinecraftClient.getInstance().getSoundManager().play((SoundInstance) TutorialMod.MY_SOUND_EVENT);
-//			MinecraftClient.getInstance().getSoundManager().play((SoundInstance) SoundIdentifier.MY_SOUND_EVENT, 1);
 			MinecraftClient.getInstance().getSoundManager().play(PositionedSoundInstance.master(TutorialMod.MY_SOUND_EVENT, 1.0F, 1.0F));
 			TutorialMod.LOGGER.info("Playing sound!");
 		} else {
